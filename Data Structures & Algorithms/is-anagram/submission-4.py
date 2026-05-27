78e1336086ec4acdc33a90f37a298ea1f7929f9f
famousfields <64670528+famousfields@users.noreply.github.com>
2026-05-27 11:39:38 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sMap = {}
+        tMap = {}
+
+        if len(s) != len(t):
+            return False
+            
+        for i in range(len(s)):
+                sMap[s[i]] = 1 + sMap.get(s[i],0)
+                tMap[t[i]] = 1 + tMap.get(t[i],0)
+           
+        for i in range(len(s)):
+            if s[i] not in tMap or sMap[s[i]] != tMap[s[i]]:
+                return False
+
+        return True 
+
+
+        
